dbda98b73f7efe2fd0cef8d904c28b47362c938f
Emilien Vaschalde <emi@macbook-air-de-emilien.home>
2023-02-21 21:17:22 +0100

    TEST: QuestionSimple and QuestionMultipe

---
+++ src/components/createTraining/QuestionMultiple/__test__/QuestionMultiple.test.tsx
@@ -0,0 +1,76 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import QuestionMultiple from "../QuestionMultiple";
+
+beforeEach(() => {
+    render(
+        <QuestionMultiple 
+            setTrainingInfo={()=>{}}
+            trainingInfo={{
+                date: [""],
+                sport: ""
+            }}
+            param="date"
+            question="a question for user"
+            choices={['choice1', 'choice2', 'choice3']}
+        />
+    )
+})
+
+describe('Question multiple testing', () => {
+    // render compo no user event
+    test('render the question', () => {
+        const question = screen.getByText(/a question for user/i)
+        expect(question).toBeInTheDocument()
+    })
+    test('render choices', () => {
+        const choices = screen.getAllByText(/choice/i)
+        expect(choices.length).toBe(3)
+    })
+    test("don't display btn valider if there is no choice selected", () => {
+        const btn = screen.queryByRole("button", { name: "Valider" })
+        expect(btn).not.toBeInTheDocument()
+    })
+
+    //user event
+    test('when user click on choice, it add active class', () => {
+        const choice = screen.getByText(/choice1/i)
+        act(() => {
+            userEvent.click(choice)
+        })
+        const choiceRe = screen.getByText(/choice1/i)
+
+        if(choiceRe.parentElement) {
+            expect(choiceRe.parentElement.classList.contains('active')).toBe(true)
+        } else {
+            expect(true).toBe(false)
+        }
+    })
+    test('when user click on active choice, it remove active class', () => {
+        const choice = screen.getByText(/choice1/i)
+        act(() => {
+            userEvent.click(choice)
+            
+        })
+        act(() => {
+            userEvent.click(choice)
+            
+        })
+        const choiceRe = screen.getByText(/choice1/i)
+
+        if(choiceRe.parentElement) {
+            expect(choiceRe.parentElement.classList.contains('active')).toBe(false)
+        } else {
+            expect(true).toBe(false)
+        }
+    })
+    test('display btn when user selected a choice', () => {
+        const choice = screen.getByText(/choice1/i)
+        act(() => {
+            userEvent.click(choice)
+        })
+        const btn = screen.getByRole('button', { name: 'Valider' })
+        expect(btn).toBeInTheDocument()
+    })
+})
---
+++ src/components/createTraining/QuestionSimple/__test__/QuestionSimple.test.tsx
@@ -0,0 +1,28 @@
+import { screen, render } from "@testing-library/react";
+import QuestionSimple from "../QuestionSimple";
+
+beforeEach(() => {
+    render(
+        <QuestionSimple 
+            setTrainingInfo={() => {}}
+            trainingInfo={{
+                date: [''],
+                sport: ''
+            }}
+            param={'sport'}
+            question={"a question for user"}
+            choices={['choice', 'choice', 'choice']}
+        />
+    )
+})
+
+describe('test QuestionSimple component', () => {
+    test('it render the question', () => {
+        const question = screen.getByText(/a question for user/i)
+        expect(question).toBeInTheDocument()
+    })
+    test('render the choices', () => {
+        const choices = screen.getAllByText(/choice/i)
+        expect(choices.length).toBe(3)
+    })
+})
